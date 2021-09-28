d6265205cda5e23bc61df82bcf4e3d6fbd779d8f
EdvinasZel <49122820+EdvinasZel@users.noreply.github.com>
2021-09-28 16:50:00 +0300

    Navigation Bar
    added Navigation bar

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/app.css') }}" />
+
+</head>
+<body>
+
+<!-- NAVIGATION BAR -->
+@include('navbar')
+<!-- BREAK -->
+<div style="width: 2000px; height: 1px; top: 66px; left: 0px; right: 0px; background-color:rgba(34, 34, 34, 0.1); position: absolute;"></div>
+<!-- WELCOME BANNER -->
+
+</body>
+</html>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,29 @@
+<div style="width: 1440px; height: 67px; left: 150px; right: 150px; background-color: #ffffff; position: absolute">
+
+    <img src="{{asset("storage/images/Vector.png")}}" alt="logo" style="position: absolute; left: 10.43%; right: 87.77%; top: 0.3%; bottom: 99.19%; width: 25.91px; height: 30.45px; top: 17.66px; left: 150.25px">
+
+    <div style="position: absolute; left: 13.11%; right: 81.23%; top: 0.21%; bottom: 99.02%; width: 81.49px; height: 46.18px">
+    <h2 style="position: absolute; left: 13.11%; right: 86.02%; top: 0.21%; bottom: 99.51%; width: 55.6px; height: 21.84px; color: #58595B; margin: 0px">Beyond<br>Body</h2>
+    </div>
+
+    <a href="#" style="position: absolute; width: 72px; height: 16px; left: 533px; top: 25px; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">FEATURES</a>
+    <a href="#" style="position: absolute; width: 63px; height: 16px; left: 644px; top: 25px; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">REVIEWS</a>
+    <a href="#" style="position: absolute; width: 71px; height: 16px; left: 753px; top: 25px; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">ABOUT US</a>
+    <a href="#" style="position: absolute; width: 39px; height: 16px; left: 868px; top: 25px; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">BLOG</a>
+
+    <div style="position: absolute; width: 40px; height: 15px; left: 1092px; top: 20px; width: 32px; height:32px; background-color: #FFFFFF">
+    <div class="dropdown">
+        <button class="dropbtn"><img src="{{asset("storage/images/748050_flag_usa_icon.png")}}"></button>
+        <div class="dropdown-content">
+            <a href="#"><img src="{{asset("storage/images/748067_flag_germany_icon.png")}}"></a>
+            <a href="#"><img src="{{asset("storage/images/748120_flag_spain_icon.png")}}"></a>
+        </div>
+    </div>
+    </div>
+
+    <button style="position: absolute; left: 80.69%; right: 10.42%; top: 0.24%; bottom: 99.13%; background-color: #FE7677; border-radius: 6px; width: 128px; height: 38px; top: 14px; left: 1162px">
+        <h style="position: absolute; height: 16px; right: 11.74%; bottom: 25%; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: center; letter-spacing: 2px; color: #FFFFFF">START NOW</h>
+    </button>
+
+</div>
+
