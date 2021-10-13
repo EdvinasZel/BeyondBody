86830515981dee67b42826342f15c180490c90ce
EdvinasZel <49122820+EdvinasZel@users.noreply.github.com>
2021-10-13 17:02:32 +0300

    update

---
+++ resources/views/contents.blade.php
@@ -0,0 +1,51 @@
+<div style="height: 598px; width: 1440px; position: absolute; margin: auto; left: 150px; right: 150px; top:3400px; background: rgba(48, 174, 198, 0.05);">
+
+    <div class="row" style="position: absolute; width: 1400px; margin:auto; height: 578px; padding-top:110px;">
+        <div class="col-6">
+
+            <div style="width:540px; height: 100px; left: 150px; position: absolute;">
+                <div class="row">
+                    <img src="{{asset("storage/images/fruit-apple 1.png")}}" alt="desc1" style="height: 46px; height: 46px;">
+                    <div style="height: 100px; width: 474px; padding-left: 20px;">
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 11px; line-height: 15px; letter-spacing: 0.06em; text-transform: uppercase; color: #30AEC6;">Recovery Plan</h>
+                        <br>
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 20px; line-height: 32px; color: #222222;">Learn how to turn failures into worthy lessons</h>
+                        <br>
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 16px; line-height: 24px; color: #222222;">Falling off track is too common to be stressing about it. A step-by-step recovery plan will be your safety net. </h>
+                    </div>
+                </div>
+            </div>
+
+            <div style="width:540px; height: 100px; left: 150px; position: absolute; top:150px;">
+                <div class="row">
+                    <img src="{{asset("storage/images/single-woman-check 1.png")}}" alt="desc2" style="height: 46px; height: 46px;">
+                    <div style="height: 100px; width: 474px; padding-left: 20px;">
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 11px; line-height: 15px; letter-spacing: 0.06em; text-transform: uppercase; color: #30AEC6;">LIFESTYLE GUIDANCE</h>
+                        <br>
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 20px; line-height: 32px; color: #222222;">Get healthy habits for good</h>
+                        <br>
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 16px; line-height: 24px; color: #222222;">Learn how to maintain a healthy lifestyle and boost your self-esteem for good. </h>
+                    </div>
+                </div>
+            </div>
+
+            <div style="width:540px; height: 100px; left: 150px; position: absolute; top:320px;">
+                <div class="row">
+                    <img src="{{asset("storage/images/shield-check-1 2.png")}}" alt="desc2" style="height: 46px; height: 46px;">
+                    <div style="height: 100px; width: 474px; padding-left: 20px;">
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 11px; line-height: 15px; letter-spacing: 0.06em; text-transform: uppercase; color: #30AEC6;">STRESS MANAGEMENT</h>
+                        <br>
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 20px; line-height: 32px; color: #222222;">Set the mind right for a healthy body</h>
+                        <br>
+                        <h style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 16px; line-height: 24px; color: #222222;">Managing weight can be overwhelming. Get all the knowledge to ease the stress and increase confidence.</h>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-6">
+            <img src="{{asset("storage/images/content.png")}}" alt="content" style="width: 646px; height: 470px; padding-top:40px;">
+        </div>
+    </div>
+
+</div>
--- resources/views/description.blade.php
+++ resources/views/description.blade.php
@@ -1,3 +1,59 @@
 <div style="height: 1004px; width: 1440px; position: absolute; margin: auto; left: 150px; right: 150px; top:1750px; background: rgba(48, 174, 198, 0.05);">
-    <h1 style="position: initial; height: 96px; padding-top:80px; left: 25.21%; right: 25.21%; top: calc(50% - 96px/2 - 1250px); font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 36px; line-height: 48px; text-align: center; color: #222222;">We made personal wellness easy of<br>managing personal wellness</h1>
+    <h1 style="position: absolute; height: 96px; padding-top:80px; left: 25.21%; right: 25.21%; font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 36px; line-height: 48px; text-align: center; color: #222222;">We made personal wellness easy of<br>managing personal wellness</h1>
+    <h1 style="position: absolute; height: 30px; left: 294px; top: 200px; width:852px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 20px; line-height: 30px; text-align: center; color: #222222; letter-spacing: 0.01em;">World's first personalized wellness book. Just for you and about you.</h1>
+
+    <div style="position: absolute; left:302px; right: 320px; top:210px; height:124px; width: 836px; margin:auto;">
+        <h1 style="position: absolute; padding-top: 50px; font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 16px; line-height: 26px; text-align: center; color: #222222;">We’ve gathered world-class nutritionists and software engineers to create an algorithm that would allow us to write a fully personalized book and set-up an assistant app in less than 24 hours after simply completing the 3-minute quiz.<br><br>To ensure 100% that it matches your lifestyle, habits, food preferences, and body goals, a professional nutritionist reviews each book before printing it. Because one-size-does-not-fit-all. You are unique.</h1>
+    </div>
+
+        <div class="row" style="position: absolute; width: 1400px; margin:auto; top:370px; height: 578px; padding-top:50px;">
+            <div class="col-6">
+
+                <div style="width:540px; height: 100px; left: 150px; position: absolute;">
+                    <div class="row">
+                        <img src="{{asset("storage/images/vegetables-salad 1.png")}}" alt="desc1" style="height: 46px; height: 46px;">
+                        <div style="height: 100px; width: 474px; padding-left: 20px;">
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 11px; line-height: 15px; letter-spacing: 0.06em; text-transform: uppercase; color: #30AEC6;">PERSONALIZED Meal plan</h>
+                            <br>
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 20px; line-height: 32px; color: #222222;">Manage wellness by eating only what you love</h>
+                            <br>
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 16px; line-height: 24px; color: #222222;">You will hand-pick every ingredient you like, and we will make your personalized plan from it. Eat only delicious meals from now on!</h>
+                        </div>
+                    </div>
+                </div>
+
+                <div style="width:540px; height: 100px; left: 150px; position: absolute; top:150px;">
+                    <div class="row">
+                        <img src="{{asset("storage/images/common-file-text-check 1.png")}}" alt="desc2" style="height: 46px; height: 46px;">
+                        <div style="height: 100px; width: 474px; padding-left: 20px;">
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 11px; line-height: 15px; letter-spacing: 0.06em; text-transform: uppercase; color: #30AEC6;">Personalized educational content</h>
+                            <br>
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 20px; line-height: 32px; color: #222222;">Manage wellness more efficiently by knowing the strengths and weaknesses of your body</h>
+                            <br>
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 16px; line-height: 24px; color: #222222;">Receive educational content based on science and selected exclusively for you, for your current state and body type.</h>
+                        </div>
+                    </div>
+                </div>
+
+                <div style="position: absolute; width: 540px; height: 0px; left: 130px; top: 330px;  border: 1px solid rgba(34, 34, 34, 0.1)"></div>
+
+                <div style="width:540px; height: 158px; left: 150px; position: absolute; top:360px;">
+                    <div class="row" style="padding-bottom:15px;">
+                        <img src="{{asset("storage/images/Screenshot 2021-02-05 at 10.52 1.png")}}" alt="desc2" style="height: 46px; height: 46px; border-radius: 20px">
+                        <div style="height: 46px; width: 474px; padding-left: 20px;">
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 14px; color: #222222;">Jessica Norman</h>
+                            <br>
+                            <h style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; color: rgba(34, 34, 34, 0.8);">48 years old</h>
+                        </div>
+                    </div>
+
+                    <h style="width:540px; height: 96px; font-family: Red Hat Text; font-style: italic; font-weight: normal; font-size: 14px; line-height: 24px; color: #222222;">I've been jumping from one diet to another, trying this and that, but nothing, nothing, nothing worked. I've been following my personalized book for 7 months now, and guess what? I've never had a single thought that I couldn't do it. It's that easy, because it's written just for me.</h>
+
+                </div>
+
+            </div>
+            <div class="col-6">
+                <img src="{{asset("storage/images/Group 189@2x.png")}}" alt="Mockup" style="width: 607px; height: 435px; padding-top:40px;">
+            </div>
+        </div>
 </div>
