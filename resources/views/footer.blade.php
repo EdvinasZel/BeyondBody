b01b18a165434b21a069e322ed78c9068c9904ec
EdvinasZel <49122820+EdvinasZel@users.noreply.github.com>
2021-10-18 18:09:33 +0300

    update finish

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,74 @@
+<div style="height: 423px; width: 1440px; position: absolute; margin: auto; left: 150px; right: 150px; top:5700px; background: #222222;">
+    <div class="row" style="width: 1140px; height: 254px; position: absolute; left: 165px; right:150px; top:45px;">
+
+        <div class="col-3">
+            <div class="row" style="height: 47px; width: 120px;">
+                <img src="{{asset("storage/images/Vector.png")}}" alt="logo" style="width: 25.91px; height: 30.45px; position: absolute; top:15px;">
+                <h4 style="position: absolute; left: 13.11%; width: 55.6px; height: 21.84px; color: #FFFFFF; padding-left: 12px;">Beyond<br>Body</h4>
+            </div>
+            <h1 style="width: 208px; height: 16px; position: absolute; top:70px; left:0px;  font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 12px; line-height: 16px; letter-spacing: 1px; color: #FFFFFF; opacity: 0.5;">2020 © ALL RIGHTS RESERVED</h1>
+        </div>
+
+        <div class="col-6">
+            <div class="row" style="height: 254px;">
+                <div class="col-4">
+                        <h1 style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 14px; line-height: 20px; letter-spacing: 1px; color: #FFFFFF">PRODUCT</h1>
+                        <br>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Features</h1></a>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Reviews</h1></a>
+                </div>
+                <div class="col-4">
+                        <h1 style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 14px; line-height: 20px; letter-spacing: 1px; color: #FFFFFF">COMPANY</h1>
+                        <br>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">About Us</h1></a>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Affiliates</h1></a>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Support</h1></a>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Contact Us</h1></a>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">FAQ</h1></a>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">General Conditions</h1></a>
+                        <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Privacy Policy</h1></a>
+                </div>
+                <div class="col-4">
+                    <h1 style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 14px; line-height: 20px; letter-spacing: 1px; color: #FFFFFF">SOCIAL</h1>
+                    <br>
+                    <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Blog</h1></a>
+                    <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Facebook</h1></a>
+                    <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Instagram</h1></a>
+                    <a href="/"><h1 style="font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 19px; letter-spacing: 1px; color: rgba(255, 255, 255, 0.8)">Twitter</h1></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <h1 style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 14px; line-height: 20px; letter-spacing: 1px; color: #FFFFFF">STAY IN TOUCH</h1>
+            <br>
+            <div style="position: absolute; width: 270px; height: 48px; background: #FFFFFF; border-radius: 12px;">
+                <div class="row" style="height: 48px;">
+                    <div class="col-8">
+                        <input type="text" id="email" name="email" placeholder="Your Email" style="height: 48px; border-radius: 12px 0px 0px 12px;">
+                    </div>
+                    <div class="col-4" style="padding: 0px 0px 0px 0px">
+                        <button style="background: #30AEC6; border-radius: 0px 12px 12px 0px; height: 48px; border-color:#30AEC6; border:0px;">
+                            <h style="height: 24px; left: 15.62%; right: 80.21%; top: calc(50% - 24px/2 - 2467px); font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 18px; line-height: 24px; text-align: center; letter-spacing: 2px; color: #FFFFFF;">SIGN UP</h>
+                        </button>
+                    </div>
+                    <br>
+                    <h1 style="width: 270px; height: 40px; position: absolute; top:70px; left:0px;  font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 20px; color: #FFFFFF; opacity: 0.8;">It is a long established fact that a reader will be distracted by the readable content of a page when.</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div style="width: 1140px; height: 1px; top:300px; background: #FFFFFF; position: absolute; left:150px; right:150px;"></div>
+
+    <div class="row" style="position: absolute; height: 36px; top:324px; left:165px; right:150px; width: 1140px">
+        <div class="col-9">
+            <h1 style="position: absolute; height: 36px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 12px; line-height: 18px; letter-spacing: 1px; color: #FFFFFF; opacity: 0.6;">DISCLAIMER: INDIVIDUAL RESULTS MAY VARY. AT THE MOMENT, OUR WEIGHT LOSS BOOK IS THE ONLY BOOK ON THE MARKET THAT OFFERS FULL PERSONALIZATION AND SHOWS A 500% INCREASE IN COPIES SOLD EVERY MONTH.</h1>
+        </div>
+        <div class="col-3">
+            <img src="{{asset("storage/images/credit-cards 1.png")}}" alt="cards" style="position: absolute; height: 24px; padding-left: 48px;">
+        </div>
+    </div>
+
+</div>
