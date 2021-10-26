93070afcb519c1f14278a2c35e9bb0597f122dd1
EdvinasZel <49122820+EdvinasZel@users.noreply.github.com>
2021-10-26 14:57:21 +0300

    commit

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,32 +1,25 @@
-<div style="width: 1440px; height: 701px; left: 150px; right: 150px; position: absolute; background-color: #ffffff; margin:auto; padding-top: 66px;">
+<div class="container">
     <div class="row" style="height: 635px; width: 1440px;">
-        <div class="col-6">
-    <img src="{{asset("storage/images/VectorArrow.png")}}" alt="logoArrow" style="position: absolute; height:89.92px;width: 86.11px; left: 520.96px; right: 51.25%; top: 129px; bottom: 95.91%; transform: rotate(339deg);">
+        <div class="col-6 left-side">
+            <div class="align-self-center">
+                <img src="{{asset("storage/images/VectorArrow.png")}}" alt="logoArrow" class="banner-logo">
 
-    <h4 style="position: absolute; width: 342px; height: 21px; left: 150px; top: 139px; font-style: normal; font-weight: bold; font-size: 16px; line-height: 21px; letter-spacing: 0.06em; text-transform: uppercase; color: #30AEC6">Join over 200,000 happy members</h4>
-        <h4 style="position: absolute;height: 116px; width: 430px; left: 150px; right: 59.72%; top: 170px; font-style: normal; font-weight: bold; font-size: 46px; line-height: 58px; color: #222222;">A revolutionary<br>way to wellness</h4>
-        <h5 style="position: absolute;width: 500px; height: 60px; left: 150px; top: 296px; font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 20px; line-height: 30px; letter-spacing: 0.01em; color: #222222;">Improve your well-being, and boost self-esteem with<br>the first fully personalized wellness book in the world.</h5>
-        <h4 style="position: absolute; width: 193px; height: 28px; left: 150px; top: 376px; font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 14px; line-height: 28px; letter-spacing: 1px; text-transform: uppercase; color: #222222;">Choose your gender:</h4>
+                <h4 class="banner-text-1">Join over 200,000 happy members</h4>
+                <h4 class="banner-text-2">A revolutionary<br>way to wellness</h4>
+                <h5 class="banner-text-3">Improve your well-being, and boost self-esteem with<br>the first fully personalized wellness book in the world.</h5>
+                <h4 class="banner-text-4">Choose your gender:</h4>
 
-        <button style="position:absolute; left: 150px; right: 75%; top: 420px; bottom: 91.1%; background: #30AEC6; border-radius: 6px; width: 210px; height: 49px; border-color:#30AEC6; border:0px;">
-            <h style="height: 24px; left: 15.62%; right: 80.21%; top: calc(50% - 24px/2 - 2467px); font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 18px; line-height: 24px; text-align: center; letter-spacing: 2px; color: #FFFFFF;">MALE</h>
-        </button>
-        <button style="position:absolute; left: 376px; right: 75%; top: 420px; bottom: 91.1%; background: #FE7677; border-radius: 6px; width: 210px; height: 49px; border-color:#FE7677; border:0px;">
-            <h style="height: 24px; left: 30.35%; right: 63.54%; top: calc(50% - 24px/2 - 2467px); font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 18px; line-height: 24px; text-align: center; letter-spacing: 2px; color: #FFFFFF;">FEMALE</h>
-        </button>
+                <button class="banner-button-1">
+                    <h class="banner-button-text">MALE</h>
+                 </button>
+                <button class="banner-button-2">
+                    <h class="banner-button-text">FEMALE</h>
+                </button>
+            </div>
         </div>
-        <div class="col-6">
-    <!-- Images -->
-    <img src="{{asset("storage/images/Screenshot 2021-03-08 at 17.29 1.png")}}" alt="image1" style="position: absolute; width: 179px; height: 239px; top: -20px; border-radius: 20px">
-    <img src="{{asset("storage/images/Screenshot 2021-03-08 at 17.24 1.png")}}" alt="image2" style="position: absolute; width: 179px; left: 209px;  border-radius: 0px 0px 20px 20px">
-    <img src="{{asset("storage/images/Screenshot 2021-05-12 at 13.07 1.png")}}" alt="image3" style="position: absolute; width: 179px; left: 408px; height: 219px;  border-radius: 0px 0px 20px 20px">
-    <img src="{{asset("storage/images/Screenshot 2021-03-08 at 17.26 1.png")}}" alt="image4" style="position: absolute; width: 179px; height: 239px; top: 229px; border-radius: 20px">
-    <img src="{{asset("storage/images/Screenshot 2021-03-08 at 17.01 1.png")}}" alt="image5" style="position: absolute; width: 179px; height: 239px; left: 209px; top: 78px; border-radius: 20px">
-    <img src="{{asset("storage/images/Screenshot 2021-05-12 at 13.08 1.png")}}" alt="image6" style="position: absolute; width: 179px; height: 239px; left: 408px; top: 229px; border-radius: 20px">
-    <img src="{{asset("storage/images/Screenshot 2021-03-08 at 17.25 2.png")}}" alt="image7" style="position: absolute; width: 179px; height: 239px; top: 479px;  border-radius: 20px 20px 0px 0px">
-    <img src="{{asset("storage/images/Screenshot 2021-05-12 at 13.08 2.png")}}" alt="image8" style="position: absolute; width: 179px; height: 239px; left: 209px; top: 330px;  border-radius: 20px">
-    <img src="{{asset("storage/images/Screenshot 2021-05-12 at 13.09 2.png")}}" alt="image9" style="position: absolute; width: 179px; height: 239px; left: 408px; top: 479px;  border-radius: 20px 20px 0px 0px">
-    <img src="{{asset("storage/images/Screenshot 2021-05-12 at 13.12 1.png")}}" alt="image10" style="position: absolute; width: 179px; left: 209px; top: 580px; height: 100px;  border-radius: 20px 20px 0px 0px">
+        <div class="col-6 right-side">
+            <div class="e-background"></div>
+{{--            <img src="{{asset("storage/images/soc-proof-new-min.jpg")}}" alt="logoArrow">--}}
         </div>
     </div>
 </div>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,93 +1,35 @@
-{{--    DESKTOP NAVIGATION BAR    --}}
-<div class="desktop-navigation">
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="{{asset("storage/images/logo-bg.png")}}" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<div style="width: 1440px; height: 67px; background-color: #ffffff; z-index: 2" class="container">
-
-    <div class="row" style="height: 67px;">
-
-        <div class="col-3">
-
-            <div class="row">
-                <div class="col-1 pt-3">
-                    <img src="{{asset("storage/images/Vector.png")}}" alt="logo">
-                </div>
-                <div class="col-11 ps-4">
-                    <h4>Beyond<br>Body</h4>
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+            <ul class="navbar-nav me-auto mx-auto">
+                <li class="nav-item px-5">
+                    <a class="nav-link" href="#"><h class="logo-text">Features</h></a>
+                </li>
+                <li class="nav-item px-5">
+                    <a class="nav-link" href="#"><h class="logo-text">Reviews</h></a>
+                </li>
+                <li class="nav-item px-5">
+                    <a class="nav-link" href="#"><h class="logo-text">About Us</h></a>
+                </li>
+                <li class="nav-item px-5">
+                    <a class="nav-link" href="#"><h class="logo-text">Blog</h></a>
+                </li>
+            </ul>
+            <div class="dropdown px-5">
+                <button class="dropbtn"><img src="{{asset("storage/images/748050_flag_usa_icon.png")}}"></button>
+                <div class="dropdown-content">
+                    <a href="#"><img src="{{asset("storage/images/748067_flag_germany_icon.png")}}"></a>
+                    <a href="#"><img src="{{asset("storage/images/748120_flag_spain_icon.png")}}"></a>
                 </div>
             </div>
-
-        </div>
-        <div class="col-6">
-            <div class="row" style="height: 67px;">
-                <div class="col text-center pt-3">
-                    <a href="#" style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">FEATURES</a>
-                </div>
-                <div class="col text-center pt-3">
-                    <a href="#" style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">FEATURES</a>
-                </div>
-                <div class="col text-center pt-3">
-                    <a href="#" style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">FEATURES</a>
-                </div>
-                <div class="col text-center pt-3">
-                    <a href="#" style="font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: right; letter-spacing: 1px; text-transform: uppercase; text-decoration: none; color: #222222">FEATURES</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-3">
-
-            <div class="row" style="height: 67px;">
-                <div class="col-5 pt-3 text-end">
-                    <div class="dropdown-l">
-                        <button class="dropbtn"><img src="{{asset("storage/images/748050_flag_usa_icon.png")}}"></button>
-                    <div class="dropdown-content">
-                        <a href="#"><img src="{{asset("storage/images/748067_flag_germany_icon.png")}}"></a>
-                        <a href="#"><img src="{{asset("storage/images/748120_flag_spain_icon.png")}}"></a>
-                    </div>
-                    </div>
-                </div>
-                <div class="col-7 pt-3 text-end">
-                    <button style="background-color: #FE7677; border-radius: 6px; width: 128px; height: 38px; top: 14px; left: 1162px">
-                        <h style="font-family: Red Hat Text ;font-style: normal; font-weight: bold; font-size: 12px; line-height: 16px; text-align: center; letter-spacing: 2px; color: #FFFFFF">START NOW</h>
-                    </button>
-                </div>
-            </div>
-
+            <button style="background-color: #FE7677; border-radius: 6px; width: 128px; height: 38px; top: 14px; left: 1162px">
+                <h class="button-text">START NOW</h>
+            </button>
         </div>
     </div>
-    </div>
-
-</div>
-
-{{--    MOBILE DEVICE NAVIGATION BAR--}}
-
-<div class="mobile-navigation">
-
-<div class="container" style="width: auto; height: 57px; background-color: #ffffff; z-index: 2">
-
-        <div class="row" style="height: 57px;">
-            <div class="col p-0">
-                <div class="row" style="height: 57px;">
-                        <div class="col-4 text-center ps-3 pt-3">
-                        <img src="{{asset("storage/images/Vector.png")}}" alt="logo" style="height: 22px; width: 20px;">
-                        </div>
-                        <div class="col-8 text-left p-0 pt-2">
-                            <h4 style="font-family: Red Hat Text;">Beyond<br>Body</h4>
-                        </div>
-                </div>
-            </div>
-            <div class="col p-0 pt-3 text-right">
-                <div class="dropdown">
-                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-                        Menu
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Link 1</a></li>
-                        <li><a class="dropdown-item" href="#">Link 2</a></li>
-                        <li><a class="dropdown-item" href="#">Link 3</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</div>
+</nav>
