21e78384ecf5a4e730c71219bf986625504343a5
EdvinasZel <49122820+EdvinasZel@users.noreply.github.com>
2021-10-27 16:43:37 +0300

    trustpilot complete

--- resources/views/trustpilot.blade.php
+++ resources/views/trustpilot.blade.php
@@ -1,119 +1,90 @@
-<div style="height: 827px; top:700px; width: 1440px; position: absolute; margin: auto; left: 150px; right: 150px; padding-top:150px;">
-    <img src="{{asset("storage/images/istockphoto-1306676517-170667a.jpg")}}" alt="world" style="position: absolute; height: 827px; width:1350px; z-index: -1; margin-top:50px">
+<div class="container trustpilot pt-5">
     <div style="z-index: 2">
-        <h3 style="position: initial;height: 96px;left: 31.25%;right: 31.25%; margin-top:60px ;font-family: Red Hat Text;font-style: normal;font-weight: bold;font-size: 36px; line-height: 48px; text-align: center; color: #303030;">Beyond Body helps people all<br>over the world</h3>
-        <br><br>
-        <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <h4 style="text-align: center; font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 40px; line-height: 53px; color: #30AEC6;">87%</h4>
-            </div>
-            <div class="col-4">
-                <h4 style="text-align: center; font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 40px; line-height: 53px; color: #30AEC6;">200,000</h4>
-            </div>
-            <div class="col-4">
-                <h4 style="text-align: center; font-family: Red Hat Text; font-style: normal; font-weight: bold; font-size: 40px; line-height: 53px; color: #30AEC6;">430,890 lbs</h4>
-            </div>
-        </div>
-        </div>
+        <img src="{{asset("storage/images/img.png")}}" alt="world" class="trustpilot-image-desktop">
+        <h3 class="trustpilot-text-1">Beyond Body helps people all<br>over the world</h3>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-4">
-                    <h4 style="font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 22px; text-align: center; color: #222222;">Of our customers would<br>recommend us</h4>
-                </div>
-                <div class="col-4">
-                    <h4 style="font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 22px; text-align: center; color: #222222;">Happy clients from 170<br>countries</h4>
-                </div>
-                <div class="col-4">
-                    <h4 style="font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 22px; text-align: center; color: #222222;">Weight lost</h4>
-                </div>
+        <div class="row pt-5 pb-5 change-to-column">
+            <img src="{{asset("storage/images/img.png")}}" alt="world" class="trustpilot-image-mobile">
+            <div class="col-4 width-max">
+                <h4 class="trustpilot-text-2">87%</h4>
+                <h4 class="trustpilot-text-3">Of our customers would<br>recommend us</h4>
+            </div>
+            <div class="col-4 width-max">
+                <h4 class="trustpilot-text-2">200,000</h4>
+                <h4 class="trustpilot-text-3">Happy clients from 170<br>countries</h4>
+            </div>
+            <div class="col-4 width-max">
+                <h4 class="trustpilot-text-2">430,890 lbs</h4>
+                <h4 class="trustpilot-text-3">Weight lost</h4>
             </div>
         </div>
 
-        <br>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-4">
+            <div class="row change-to-column">
+                <div class="col-4 width-max">
                     <div class="card" style="padding: 22px 22px 22px 22px">
-                        <div class="row" style="padding-left: 15px;">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                        </div>
-                        <h5 style="padding-top: 11px; height: 26px; left: 172px; top: 1207px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 26px; color: #222222;">Life-changing</h5>
-                        <p style="padding-top: 11px;width: 316px; height: 66px; left: 172px; top: 1241px; font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 22px; color: #222222;">I have already begun the program, and I see changes! I'm very happy with the setum of the book and how easy it is to follow.</p>
-                        <br><br>
+                        <img src="{{asset("storage/images/trustpilot-trust-pilot-stars-png-removebg-preview.png")}}" alt="world" class="trustpilot-stars">
+                        <h5 class="trustpilot-text-4">Life-changing</h5>
+                        <p class="trustpilot-text-5">I have already begun the program, and I see changes! I'm very happy with the setum of the book and how easy it is to follow.</p>
+                        <br>
                         <div class="row">
                             <div class="col-6">
-                                <h5 style="height: 26px; left: 172px; top: 1357px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 26px; color: #222222;">Desi Gonzalez</h5>
+                                <h5 class="trustpilot-text-4">Desi Gonzalez</h5>
                             </div>
-                            <div class="col-6" style="padding-left: 11px">
-                                <div class="row">
-                                <img src="{{asset("storage/images/green-removebg-preview.png")}}" alt="trust" style="width: 18.75px; height: 18.75px">
-                                <p style="font-family: Lato; font-style: normal; font-weight: normal; font-size: 14px; line-height: 24px; color: #00B67A;">Verified Customer</p>
-                                </div>
-                                </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card" style="padding: 22px 22px 22px 22px">
-                        <div class="row" style="padding-left: 15px;">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                        </div>
-                        <h5 style="padding-top: 11px; height: 26px; left: 172px; top: 1207px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 26px; color: #222222;">The quality of the product of perfect</h5>
-                        <p style="padding-top: 11px;width: 316px; height: 66px; left: 172px; top: 1241px; font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 22px; color: #222222;">The quality of the product is perfect, the information is spot on, the program overall is great, Their customer support is AMAZING, shipping was FAST. Overall, I’m very pleased with my investment!</p>
-                        <br><br>
-                        <div class="row">
                             <div class="col-6">
-                                <h5 style="height: 26px; left: 172px; top: 1357px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 26px; color: #222222;">Veronica Warner</h5>
-                            </div>
-                            <div class="col-6" style="padding-left: 11px">
-                                <div class="row">
-                                    <img src="{{asset("storage/images/green-removebg-preview.png")}}" alt="trust" style="width: 18.75px; height: 18.75px">
-                                    <p style="font-family: Lato; font-style: normal; font-weight: normal; font-size: 14px; line-height: 24px; color: #00B67A;">Verified Customer</p>
+                                <div style="display: flex; padding-top: 15px;">
+                                <img src="{{asset("storage/images/green-removebg-preview.png")}}" alt="trust" class="trustpilot-verified">
+                                <p class="trustpilot-text-6">Verified Customer</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+
+                <div class="col-4 width-max">
                     <div class="card" style="padding: 22px 22px 22px 22px">
-                        <div class="row" style="padding-left: 15px;">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                            <img src="{{asset("storage/images/Trust.png")}}" alt="trust" style="width: 18.75px; height: 18.75px; padding-left: 1px">
-                        </div>
-                        <h5 style="padding-top: 11px; height: 26px; left: 172px; top: 1207px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 26px; color: #222222;">Definitely Worth the Money</h5>
-                        <p style="padding-top: 11px;width: 316px; height: 66px; left: 172px; top: 1241px; font-family: Red Hat Text; font-style: normal; font-weight: normal; font-size: 14px; line-height: 22px; color: #222222;">Love the book! It has helped me with workout plans and ideas, as well as new food ideas and combos. I absolutely love it! The motivational bits are also a great addition, and they really help motivate you.</p>
-                        <br><br>
+                        <img src="{{asset("storage/images/trustpilot-trust-pilot-stars-png-removebg-preview.png")}}" alt="world" class="trustpilot-stars">
+                        <h5 class="trustpilot-text-4">The quality of the product of perfect</h5>
+                        <p class="trustpilot-text-5">The quality of the product is perfect, the information is spot on, the program overall is great, Their customer support is AMAZING, shipping was FAST. Overall, I’m very pleased with my investment!</p>
+                        <br>
                         <div class="row">
                             <div class="col-6">
-                                <h5 style="height: 26px; left: 172px; top: 1357px; font-family: Red Hat Text; font-style: normal; font-weight: 500; font-size: 16px; line-height: 26px; color: #222222;">Sonahri</h5>
+                                <h5 class="trustpilot-text-4">Veronica Warner</h5>
                             </div>
-                            <div class="col-6" style="padding-left: 11px">
-                                <div class="row">
-                                    <img src="{{asset("storage/images/green-removebg-preview.png")}}" alt="trust" style="width: 18.75px; height: 18.75px">
-                                    <p style="font-family: Lato; font-style: normal; font-weight: normal; font-size: 14px; line-height: 24px; color: #00B67A;">Verified Customer</p>
+                            <div class="col-6">
+                                <div style="display: flex; padding-top: 15px;">
+                                    <img src="{{asset("storage/images/green-removebg-preview.png")}}" alt="trust" class="trustpilot-verified">
+                                    <p class="trustpilot-text-6">Verified Customer</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <img src="{{asset("storage/images/kissclipart-trust-pilot-logo-png-clipart-trustpilot-customer-r-d094b339db3ece35-removebg-preview.png")}}" alt="trustLogo" style="position: absolute;height: 76px;left: 44%;right: 31.25%; margin-top:60px;">
+                <div class="col-4 width-max">
+                    <div class="card" style="padding: 22px 22px 22px 22px">
+                        <img src="{{asset("storage/images/trustpilot-trust-pilot-stars-png-removebg-preview.png")}}" alt="world" class="trustpilot-stars">
+                        <h5 class="trustpilot-text-4">Definitely Worth the Money</h5>
+                        <p class="trustpilot-text-5">Love the book! It has helped me with workout plans and ideas, as well as new food ideas and combos. I absolutely love it! The motivational bits are also a great addition, and they really help motivate you.</p>
+                        <br>
+                        <div class="row">
+                            <div class="col-6">
+                                <h5 class="trustpilot-text-4">Sonahri</h5>
+                            </div>
+                            <div class="col-6">
+                                <div style="display: flex; padding-top: 15px;">
+                                    <img src="{{asset("storage/images/green-removebg-preview.png")}}" alt="trust" class="trustpilot-verified">
+                                    <p class="trustpilot-text-6">Verified Customer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        <div class="row justify-content-center">
+        <img src="{{asset("storage/images/kissclipart-trust-pilot-logo-png-clipart-trustpilot-customer-r-d094b339db3ece35-removebg-preview.png")}}" alt="trustLogo" class="trustpilot-logo">
+        </div>
 
     </div>
 </div>
